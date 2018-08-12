f8d13483463caf05ee7b438267bdb7d5fe4e4f0d
spawnmarvel <espenkleivane@gmail.com>
2018-08-12 12:59:06 +0200

    Base.html added route / Digitial O migrate

--- elo/views.py
+++ elo/views.py
@@ -13,6 +13,11 @@ def index(request):
     return render(request, "elo/index.html", context)
 
 
+def about(request):
+    data = Note.objects.count()
+    context = {"data": data}
+    return render(request, "elo/about.html", context)
+
 def test():
     n = Note(raw_text = "This is a test", sentences=1)
     n.save()
