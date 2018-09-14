902950cba718212e357a920de40f0ecc4f78a96e
spawnmarvel <espenkleivane@gmail.com>
2018-09-14 19:19:55 +0200

    changed IR functions and added create index

--- elo/elo_interface/util.py
+++ elo/elo_interface/util.py
@@ -3,21 +3,42 @@ from elo_interface import stop_words
 
 stop_list = stop_words.stopwords
 
-def wordsToDict(words):
-    freq = [words.count(w) for w in words]
-    return dict(zip(words, freq))    
+def textToDict(words):
+    # freq = [words.count(w) for w in words]
+	freq = []
+	for w in words:
+	    freq.append(words.count(w))
+	return dict(zip(words, freq))    
 
-def wordsFreqSort(words):
-    aux = [(words[key], key) for key in words]
-    aux.sort()
-    aux.reverse()
-    return aux
+def textFreqSort(words):
+    # aux = [(words[key], key) for key in words]
+	aux = []
+	for k in words:
+	    item = (words[k], k)
+	    aux.append(item)
+	aux.sort()
+	aux.reverse()
+	return aux
 
-def removeStop(words):
+def removeStopWords(words):
     global stop_list
-    return[w for w in words if w not in stop_list]
+    result = []
+    for w in words:
+	    if w not in stop_list:
+		    result.append(w)
+			
+    return result	
+    # return[w for w in words if w not in stoplist]
 
 	
 def stripNonAlphaNum(text):
     return re.compile(r'\W+', re.UNICODE).split(text)
+
+
+def createIndex(words):
+    tmp = stripNonAlphaNum(words)
+    rm_stop = removeStopWords(tmp)
+    to_dict = textToDict(rm_stop)
+    index = textFreqSort(to_dict)
+    return index
 
